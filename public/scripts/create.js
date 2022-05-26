eabe64da2831ae233542d427dc09e50a09102524
Danny Rodriguez <rod.dr.dan@gmail.com>
2022-05-26 13:10:33 -0700

    editor complete

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -27,11 +27,12 @@ function drawGrid() {
     cell.className = "cell"
     cell.id = `${i + 1}`
     // cell.innerText = cell.id
+    //
     cell.addEventListener("click", event => {
       if (clickMode.innerText === "box") {
         cell.style.background = "yellow"
-        cell.innerText = ""
-        if (selectedCell != undefined) {
+
+        if (selectedCell != undefined && selectedCell !== cell) {
           selectedCell.style.background = "white"
         }
         selectedCell = cell
@@ -61,26 +62,50 @@ for (var i = 0; i < dropdownContent.length; i++) {
 }
 
 clickMode.addEventListener("click", event => {
+  // changing modes
   if (clickMode.innerText === "box") {
     clickMode.innerText = "type"
+    // unselecting the cell
+    selectedCell.style.background = "white"
   } else if (clickMode.innerText === "type") {
     clickMode.innerText = "box"
   }
+  selectedCell = undefined
 })
 
 window.addEventListener("keydown", event => {
   if (selectedCell === undefined) {
     return
   }
-  selectedCell.innerText = event.key
-  // This for loop selects the next cell across
-  for (var i = parseInt(selectedCell.id) + 1; i <= size * size; i++) {
-    let nextCell = document.getElementById(`${i}`)
-    if (nextCell.style.background === "white" || nextCell.style.background === "") {
-      selectedCell.style.background = "white"
-      selectedCell = nextCell
-      selectedCell.style.background = "yellow"
-      break
+  if (event.key.charCodeAt(0) >= 65 && event.key.charCodeAt(0) <= 122 && event.key.length === 1) {
+    //talk about this
+    selectedCell.innerText = event.key.toUpperCase()
+    // This for loop selects the next cell across
+    for (var i = parseInt(selectedCell.id) + 1; i <= size * size; i++) {
+      let nextCell = document.getElementById(`${i}`)
+      if (nextCell.style.background === "white" || nextCell.style.background === "") {
+        selectedCell.style.background = "white"
+        selectedCell = nextCell
+        selectedCell.style.background = "yellow"
+        break
+      }
+    }
+  } else if (event.key === "Backspace") {
+    let foundPrev = false
+    for (var i = parseInt(selectedCell.id) - 1; i >= 1; i--) {
+      let prevCell = document.getElementById(`${i}`)
+      if (prevCell.style.background === "white" || prevCell.style.background === "") {
+        selectedCell.style.background = "white"
+        selectedCell.innerText = ""
+        selectedCell = prevCell
+        selectedCell.style.background = "yellow"
+        foundPrev = true
+        break
+      }
+    }
+    // could not find previous cell
+    if (!foundPrev) {
+      selectedCell.innerText = ""
     }
   }
   // Todo: Write code for selecting cell down. add a button. click functionality to change mode and text. Depending on mode it chooses which loop to run to select the next cell. Use if statements to determine which you're on
