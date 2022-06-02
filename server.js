4df0611e44709e8d3b2adbaeba302eb3ed8457e5
Danny Rodriguez <rod.dr.dan@gmail.com>
2022-06-02 16:53:01 -0700

    MongoDB added

--- server.js
+++ server.js
@@ -1,20 +1,94 @@
-const express = require("express") // create basic express server
-const exphbs = require("express-handlebars") // create express-handlebars
 const path = require("path")
-const dotenv = require("dotenv") // store config of code environment
+const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+// const morgan = require("morgan")
+const exphbs = require("express-handlebars")
+// const methodOverride = require("method-override")
+const passport = require("passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+const connectDB = require("./config/db")
+
+// Load config
+dotenv.config({ path: "./config/config.env" })
+
+//passport config
+require("./config/passport")(passport)
+
+connectDB()
 
 const app = express()
-const PORT = process.env.PORT || 3000 // process.env navigates variables in config.env
 
-// Handlebars
+// Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// //Method override
+// app.use(
+//   methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === "object" && "_method" in req.body) {
+//       // look in urlencoded POST bodies and delete it
+//       let method = req.body._method
+//       delete req.body._method
+//       return method
+//     }
+//   })
+// )
+
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"))
+// }
+
+const PORT = process.env.PORT || 5000
+
+//Handbars Helpers
+// const { formatDate, stripTags, truncate, editIcon, select } = require("./helpers/hbs")
+
+//Handelbars
+// app.engine(
+//   ".hbs",
+//   exphbs.engine({
+//     helpers: {
+//       formatDate,
+//       stripTags,
+//       truncate,
+//       editIcon,
+//       select
+//     },
+//     defaultLayout: "main",
+//     extname: ".hbs"
+//   })
+// )
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: "hbs" }))
-// set ".hbs" as handlebars" extension
-// set default layout as "main" (views/layouts/main.hbs)
-
 app.set("view engine", ".hbs")
 
-app.use(express.static(path.join(__dirname + "/public")))
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+  })
+)
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set Global Var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+//Static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+//Routes
 app.use("/", require("./routes/index"))
+app.use("/auth", require("./routes/auth"))
 app.use("/solve", require("./routes/solve"))
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
