e84c56138268c2441d37263f1c43d98988698522
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-02-07 16:28:42 -0800

    engines

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-const express = require("express")
-const router = express.Router()
-const { ensureAuth, ensureGuest } = require("../middleware/auth")
-const Crossword = require("../models/Crossword")
+const express = require("express");
+const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Crossword = require("../models/Crossword");
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // @desc home page
 // @route Get /
@@ -10,8 +11,8 @@ router.get("/", ensureAuth, async (req, res) => {
     layout: "main",
     title: "Home | Crosswordia",
     name: req.user.firstName
-  })
-})
+  });
+});
 
 // @desc create page
 // @route Get /create
@@ -19,8 +20,8 @@ router.get("/create", ensureAuth, async (req, res) => {
   return res.render("create", {
     layout: "main",
     title: "Create | Crosswordia"
-  })
-})
+  });
+});
 
 // @desc login page
 // @route Get /login
@@ -28,49 +29,49 @@ router.get("/login", ensureGuest, async (req, res) => {
   return res.render("login", {
     layout: "mainGuest",
     title: "Login | Crosswordia"
-  })
-})
+  });
+});
 
 // @desc Delete function
 // @route Get /delete(:id)
 router.get("/delete/:id", ensureAuth, async (req, res) => {
   Crossword.findByIdAndDelete(req.params.id, (err, doc) => {
     if (!err) {
-      return res.redirect("/user/" + req.user.id)
+      return res.redirect("/user/" + req.user.id);
     } else {
-      return console.log("Failed to Delete user Details: " + err)
+      return console.log("Failed to Delete user Details: " + err);
     }
-  })
-})
+  });
+});
 
 // @desc gallery page
 // @route Get /gallery
 router.get("/gallery", ensureAuth, async (req, res) => {
   return res.render("gallery", {
     title: "Gallery | Crosswordia"
-  })
-})
+  });
+});
 
 // @desc user page
 // @route Get /user
 router.get("/user", ensureAuth, async (req, res) => {
   try {
-    return res.redirect(`/user/` + req.user.id)
+    return res.redirect(`/user/` + req.user.id);
   } catch (error) {
-    return console.error(error)
+    return console.error(error);
   }
-})
+});
 
 // @desc about page
 //@ route Get /about
 router.get("/about", async (req, res) => {
-  let layoutValue
-  req.isAuthenticated() ? (layoutValue = "main") : (layoutValue = "mainGuest")
+  let layoutValue;
+  req.isAuthenticated() ? (layoutValue = "main") : (layoutValue = "mainGuest");
   return res.render("about", {
     layout: layoutValue,
     title: "About | Crosswordia"
-  })
-})
+  });
+});
 
 router.post("/crossword", ensureAuth, async (req, res) => {
   try {
@@ -80,26 +81,26 @@ router.post("/crossword", ensureAuth, async (req, res) => {
       size: req.body.size,
       solution: req.body.solution,
       hints: req.body.hints
-    }
-    const newCrossword = await Crossword.create(toPost)
-    return res.redirect(`/solve/` + newCrossword._id)
+    };
+    const newCrossword = await Crossword.create(toPost);
+    return res.redirect(`/solve/` + newCrossword._id);
   } catch (error) {
-    return console.error(error)
+    return console.error(error);
   }
-})
+});
 
 router.post("/dictionary", async (req, res) => {
-  const word = JSON.stringify(req.body.word)
-  const dictApi = process.env.DICTIONARYAPI_KEY
+  const word = JSON.stringify(req.body.word);
+  const dictApi = process.env.DICTIONARYAPI_KEY;
   try {
     await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${dictApi}`)
       .then(res => res.json())
       .then(result => {
-        return res.json(result)
-      })
+        return res.json(result);
+      });
   } catch (error) {
-    return console.error("/dictionary error: " + error)
+    return console.error("/dictionary error: " + error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
